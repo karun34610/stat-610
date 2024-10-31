e4b9e328752eac630673a9e82a9cec249425fdc9
Karun <karun34610@gmail.com>
2024-10-31 00:41:20 -0400

    tried to simplify using sweep

--- llr_functions.R
+++ llr_functions.R
@@ -12,8 +12,8 @@ compute_f_hat <- function(z,x,y,omega) {
   
   Wz = make_weight_matrix(z, x, omega) 
   X = make_predictor_matrix(x)
-  XWz <- apply(X, 2, function(col, i){col * Wz}, Wz)
-  f_hat = c(1, z) %*% solve(t(X) %*% XWz) %*% t(X) %*% yWz
+  XWz <- sweep(X, 1, Wz, "*")
+  f_hat = c(1, z) %*% solve(t(X) %*% XWz) %*% t(X) %*% (y*Wz)
   return(f_hat)
   
 }
